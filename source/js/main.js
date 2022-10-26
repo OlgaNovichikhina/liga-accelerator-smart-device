1f0fbf34cc31a4150ff778db218d2c01a9f95cad
OlgaNovichikhina <97948178+OlgaNovichikhina@users.noreply.github.com>
2022-10-26 20:14:01 +0300

    футер

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {showMoreText} from './modules/about';
+import {scrollPage} from './modules/scroll';
 
 // ---------------------------------
 
@@ -17,6 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   showMoreText();
+  scrollPage();
   window.addEventListener('load', () => {
     initModals();
   });
